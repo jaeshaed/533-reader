c3b3d08e16e44ad75e7c44853de7397c4681a5d5
sergey <ssssssssss@mail.ru>
2025-03-11 13:37:41 +0300

    Удалить

--- core/BOOKW.py
+++ core/BOOKW.py
@@ -1,11 +1,5 @@
 import os
 import shutil
 
-def picnik():
-    # os.remove("C:/Users/300-2/533-reader/12/32")
-    # with open(f"Книги/{"book_name"}.txt", "w") as book_file:
-    #     book_file.write("book_text")
-    shutil.rmtree(f"Книги/{"book_name"}")
-
- 
- 
+def delete_book(book_name):
+    shutil.rmtree(os.path.join("Книги", book_name))

6fd59bebd8678b366cb7fdebcff0e8ab354ac238
Игорь Шатров <kakashkephobia@lexa.com>
2025-03-11 12:13:51 +0300

    первая версия list_book

--- core/list_books.py
+++ core/list_books.py
@@ -1,5 +1,11 @@
 import os
 
 
-def list_books(filter=None):
-    return os.listdir("Книги")
+directory = "Книги"
+name = input("введите книгу: ")
+def list_books(filter = None):
+    for filename in os.listdir("Книги"):
+        filename_with_path = os.path.join("Книги", filename)
+        if os.path.isdir(filename_with_path):
+            if name in filename_with_path:
+                print(filename_with_path)

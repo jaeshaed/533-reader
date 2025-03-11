5a4ec8e03239870466488a9da58df1c2ce2122fc
Данила Ткаченко <qwerty52@goida.com>
2025-03-11 12:25:42 +0300

    cделаl добавление книг в библиотеку

--- core/add_book.py
+++ core/add_book.py
@@ -1,10 +1,3 @@
 def add_book(book_name, book_text):
-
-
     with open(f"Книги/{book_name}.txt", "w") as book_file:
         book_file.write(book_text)
-        
-        
-book_title = input("Введите название книги:")
-book_content = input("Введите текст книги:")
-add_book(book_title, book_content)

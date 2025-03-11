ee25add27c5e65413582d2bcfecf29df0890d11a
Игорь Шатров <kakashkephobia@lexa.com>
2025-03-11 14:18:38 +0300

    сделал задачу

--- core/list_books.py
+++ core/list_books.py
@@ -1,11 +1,13 @@
 import os
 
 
-directory = "Книги"
-spisok = []
-def list_books(filter = None):
+def list_books(search = None):
+    spisok = []
     for filename in os.listdir("Книги"):
         filename_with_path = os.path.join("Книги", filename)
         if os.path.isdir(filename_with_path):
-            if filter in filename_with_path:
+            if search is None:
                 spisok.append(filename_with_path)
+            elif search in filename_with_path:
+                spisok.append(filename_with_path)
+    return spisok

be34a7adeb92490d3d5137fbd86d552932dafbbe
jaeshaed <36546964+jaeshaed@users.noreply.github.com>
2025-03-11 19:26:59 +0300

    UI будет на Kivy.

--- UI.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-
-root = Tk()  # создаем корневой объект - окно
-root.title("Book-Reader")  # устанавливаем заголовок окна
-root.geometry("300x250")  # устанавливаем размеры окна
-
-root.mainloop()
